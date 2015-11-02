891cf7b274ee68ba93cb280bfba869b2c1d7e30e
Richard Williamson <richard@svds.com>
2015-11-02 20:28:05 +0000

    Fix path issues

--- bin/LoadImpalaTable.sh
+++ bin/LoadImpalaTable.sh
@@ -3,6 +3,6 @@
 --packages "org.kududb:kudu-client:0.5.0,org.apache.spark:spark-streaming_2.10:1.5.1,"\
 "org.apache.spark:spark-streaming-kafka_2.10:1.5.1,org.apache.spark:spark-sql_2.10:1.5.1" \
 --class com.svds.kudumeetup.KuduMeetupParquetLoad \
-target/scala-2.10/kudu-meetup_2.10-1.0.jar \
+../target/scala-2.10/kudu-meetup_2.10-1.0.jar \
 /user/ubuntu/meetup_parquet \
-file:/data4/meetupstream/meetupstream.json
+file:/mnt/meetupstream/meetupstream.json
--- bin/RunKuduMeetupStreamingPrediction.sh
+++ bin/RunKuduMeetupStreamingPrediction.sh
@@ -3,7 +3,7 @@
 --packages "org.kududb:kudu-client:0.5.0,org.apache.spark:spark-streaming_2.10:1.5.1,"\
 "org.apache.spark:spark-streaming-kafka_2.10:1.5.1,org.apache.spark:spark-sql_2.10:1.5.1" \
 --class com.svds.kudumeetup.KuduMeetupStreamingPrediction \
-target/scala-2.10/kudu-meetup_2.10-1.0.jar \
+../target/scala-2.10/kudu-meetup_2.10-1.0.jar \
 localhost \
 localhost:9092
 
